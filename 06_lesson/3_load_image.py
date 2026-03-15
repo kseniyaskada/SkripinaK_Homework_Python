c332ee5cd87f93c1b477d81348d6592e238a79e1
kseniyaskada <kseniyaksenya@gmail.com>
2026-03-15 14:45:42 +0300

    Домашка 6.1

--- 06_lesson/3_load_image.py
+++ 06_lesson/3_load_image.py
@@ -1,12 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Edge()
 
-driver.implicitly_wait(10)
-
 driver.get(
     "https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
+
+wait = WebDriverWait(driver, 20)
+
+wait.until(EC.visibility_of_element_located
+           ((By.CSS_SELECTOR, '#landscape')))
 
 src = driver.find_element(By.CSS_SELECTOR, '#award').get_attribute("src")
 print(src)
